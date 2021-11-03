ff5d16813909dc9be168cff2448df1f2e9332d43
mianazeemdaula <mianazeemdaula@gmail.com>
2021-11-03 12:42:49 +0500

    setup

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'Galeedu',
+    'title' => 'Yummy Farms',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>Geleedu</b>',
+    'logo' => '<b>Yummy Farm</b>',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'AdminLTE',
+    'logo_img_alt' => 'Yummy Farm',
 
     /*
     |--------------------------------------------------------------------------
