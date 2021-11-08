3a8431200cdd5126d349445858605678fb227d0c
mianazeemdaula <mianazeemdaula@gmail.com>
2021-11-08 11:01:41 +0500

    validator

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
