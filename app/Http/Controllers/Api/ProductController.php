3c6167a66f9824075634621e3411a71e4212e003
mianazeemdaula <mianazeemdaula@gmail.com>
2021-11-08 11:02:52 +0500

    exception

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use Illuminate\Support\Facades\Validator;
+use Exception;
 
 class ProductController extends Controller
 {
