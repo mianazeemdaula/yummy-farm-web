f26d5db33cf30ebbc9c0362b1bec9b0973ae2924
mianazeemdaula <mianazeemdaula@gmail.com>
2021-11-15 19:25:15 +0500

    haram

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -8,6 +8,7 @@ use App\Models\Product;
 use App\Models\Category;
 use App\Models\MeatProducts;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 use Exception;
 
 class ProductController extends Controller
