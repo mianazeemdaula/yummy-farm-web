fd9c9fbd8ff8656d9e10a6025212288b97c977d0
mianazeemdaula <mianazeemdaula@gmail.com>
2021-12-06 09:07:42 +0500

    valid

--- app/Http/Controllers/Api/DeliveryChargesController.php
+++ app/Http/Controllers/Api/DeliveryChargesController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\DeliveryCharge;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class DeliveryChargesController extends Controller
 {
